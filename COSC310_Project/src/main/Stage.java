c9f8796bdb805c660c6cfda38898751005a5858d
BuildTools <unconfigured@null.spigotmc.org>
2020-03-11 18:12:12 -0700

    Backbone v.1

---
+++ COSC310_Project/src/main/Stage.java
@@ -0,0 +1,10 @@
+package main;
+
+public interface Stage {
+
+	//main Stage interface
+	public boolean start(); //return false if failed
+	public void printFailMessage(); //print a failed message if failed 3 times
+	
+	
+}
---
+++ COSC310_Project/src/stages/SampleStage.java
@@ -0,0 +1,21 @@
+package stages;
+import java.util.Scanner;
+
+import main.Stage;
+public class SampleStage implements Stage{
+
+	public boolean start() {
+		System.out.println("this is a sample stage. Enter 'c' to continue or anything else to fail");
+		Scanner in = new Scanner(System.in);
+		if(in.next().equalsIgnoreCase("c")) {
+			System.out.println("stage complete.");
+			return true;
+		}
+		return false;
+	}
+	
+	public void printFailMessage() {
+		System.out.println("Sample stage failed. Returning to main for another \"agent\"");
+	}
+	
+}
