5ec6b37d7c3f1c1ee5fc491b7960a7d47e681c65
a1v1n1 <qwertwatt@gmail.com>
2020-04-04 18:45:10 -0700

    Added unknown topic prompts

--- COSC310_Project/src/stages/IntroStage.java
+++ COSC310_Project/src/stages/IntroStage.java
@@ -6,11 +6,12 @@ import main.Stage;
 public class IntroStage implements Stage{
 	
 	public boolean start(Bot b) {
+		int unin = 0;
 		Bot bot = b;
 		System.out.println("");
 		Scanner in = new Scanner(System.in);
 		System.out.println(cannedGreets1[(int)(Math.random()*cannedGreets1.length)] +bot.name 
-				+cannedGreets4[(int)(Math.random()*cannedGreets4.length)]);//Greet user, ask about medical help
+				+cannedGreets2[(int)(Math.random()*cannedGreets2.length)]);//Greet user, ask about medical help
 		boolean medX = false;
 		while(!medX) {
 			String answer = in.next();
@@ -21,8 +22,16 @@ public class IntroStage implements Stage{
 			if(answer.equalsIgnoreCase("yes")) {
 				medX = true;
 			}
-			else
-				System.out.println(cannedYesNo[(int)(Math.random()*cannedYesNo.length)]);//not yes or no
+			else {
+				if(unin<5) {
+					System.out.println(cannedTopic[(int)(Math.random()*cannedTopic.length)]);//not yes or no
+				}
+				else {
+					System.out.println("It seems that I cannot help you, have a good day.");
+					return false;
+				}
+				unin++;
+			}
 		}
 		return true;//continue to ImmediateStage
 	}
@@ -31,13 +40,15 @@ public class IntroStage implements Stage{
 		System.out.println("Intro stage failed. Returning to main for another \"agent\"");
 	}
 	
+	
 	//Resources
 	String[] cannedGreets1 = {"Hello, my name is ", "Hi there, I'm ", "Greetings, I'm "};
-	String[] cannedGreets2 = {", may I ask your for your name?", ", what's your name?", ", who am I speaking to?"};
-	String[] cannedGreets3 = {"Hello ", "Hi ", "Greetings "};
-	String[] cannedGreets4 = {", did you need medical attention?", ", are you looking for medical attention?",
+	String[] cannedGreets2 = {", did you need medical attention?", ", are you looking for medical attention?",
 			", do you need medical help?"};
-	String[] cannedYesNo = {"Pardon me, was that a yes or a no?", "Sorry, I didn't quite catch that. Was that yes or no?"};
+	String[] cannedTopic = {"Sorry, I don't know much about that topic. Could I help you with getting medical help or [TOPIC TWO]?",
+			"I'm not trained on that topic. Do you want help with medical attention or [TOPIC TWO]?", 
+			"I can only help you in getting medical attention and [TOPIC TWO]. Would you like help with that?", 
+			"I'm sorry did you need help with getting medical attention or [TOPIC TWO]?",
+			"That's not something I'm familiar with. Can I help you with getting medical help or [TOPIC TWO]?"};
 	
 }
-
